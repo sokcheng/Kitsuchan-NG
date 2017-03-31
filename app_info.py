5becbdfda12b85f9ab3102d1c00f64956d5bd51f
n303p4 <n303p4@users.noreply.github.com>
2017-03-31 19:45:08 -0400

    Updated app info

--- app_info.py
+++ app_info.py
@@ -6,5 +6,5 @@ Yes, it's slightly bad practice to put this in a Python file."""
 
 APP_NAME = "kitsuchan-ng"
 APP_URL = "https://github.com/n303p4/kitsuchan-ng"
-APP_VERSION = (0, 1, 0)
+APP_VERSION = (0, 2, 0)
 APP_VERSION_STRING = "%s.%s.%s" % APP_VERSION
