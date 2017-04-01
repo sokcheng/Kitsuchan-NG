918a8a31fcf08cd2525e03c7c38e413f9f2ac7ca
n303p4 <n303p4@users.noreply.github.com>
2017-04-01 04:07:30 -0400

    This file wasn't actually needed.

--- cogs/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# https://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python#1057534
-from os.path import dirname, basename, isfile
-import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
