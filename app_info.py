9f15c042be604e9e0f64de2a5b25af7a18282384
n303p4 <n303p4@users.noreply.github.com>
2017-04-02 21:45:12 -0400

    Version bump.

--- app_info.py
+++ app_info.py
@@ -8,5 +8,5 @@ APP_NAME = "kitsuchan-ng"
 APP_URL = "https://github.com/n303p4/kitsuchan-ng"
 APP_DESCRIPTION = ("A Discord bot that fetches anime images and does other things. It's modular "
                    "and extensible, so you can add addtional functions to it quite easily.")
-APP_VERSION = (0, 2, 0)
+APP_VERSION = (0, 2, 1)
 APP_VERSION_STRING = "%s.%s.%s" % APP_VERSION
