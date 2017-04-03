4c7ae6dd7351a8567c3ab3372d55347d9f697de5
n303p4 <n303p4@users.noreply.github.com>
2017-04-03 15:04:51 -0400

    [app_info.py] Description updated.

--- app_info.py
+++ app_info.py
@@ -6,7 +6,9 @@ Yes, it's slightly bad practice to put this in a Python file."""
 
 NAME = "kitsuchan-ng"
 URL = "https://github.com/n303p4/kitsuchan-ng"
-DESCRIPTION = ("A Discord bot that fetches anime images and does other things. It's modular "
-                   "and extensible, so you can add addtional functions to it quite easily.")
+DESCRIPTION = ("This is a running instance of `%s`, a modular Discord bot. Originally designed "
+               "with anime images and basic utility in mind, it has become a fairly flexible "
+               "bot with a high level of extensibility. `%s` surely isn't finished yet; please"
+               "report any bugs you observe!") % (NAME, NAME,)
 VERSION = (0, 2, 2)
 VERSION_STRING = "%s.%s.%s" % VERSION
