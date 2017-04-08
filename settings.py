c96f6a5489026de54cfe426f6686168764dc488f
n303p4 <n303p4@users.noreply.github.com>
2017-04-08 00:54:33 -0400

    [settings.py] Fixed load command. Updated docstrings.

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ import os
 import json
 import logging
 
-FILENAME = "config.json"
+FILE_CONFIG = "config.json"
 FOLDER_COGS = "cogs"
 DEFAULT_EXTENSIONS = []
 # Dynamically create the default list of cogs.
@@ -20,16 +20,23 @@ logger = logging.getLogger(__name__)
 
 manager = {}
 
-def load(filename:str=FILENAME):
-    """Load config settings from filename.
+def load(filename:str=FILE_CONFIG):
+    """Load config from a file. If no filename is specified, it modifies settings.manager.
+    Otherwise, it returns the config it just loaded.
     
-    filename - The name of the file you wish to write to."""
+    filename - The name of the file you wish to write to, defaults to FILE_CONFIG."""
     logger.info("Loading config file.")
     try:
         with open(filename) as f:
             new_settings = json.load(f)
-        for key, value in new_settings.items():
-            manager[key] = value
+        # Treat things differently here. If the filename is the stock FILE_CONFIG, then the load
+        # command should modify the global manager and return None.
+        if filename == FILE_CONFIG:
+            for key, value in new_settings.items():
+                manager[key] = value
+        # Otherwise, it returns the new_settings so they can be used in a cog-based extension.
+        else:
+            return new_settings
     except FileNotFoundError as error:
         logger.warning("Config file not found!")
         raise error
@@ -40,11 +47,12 @@ def load(filename:str=FILENAME):
         logger.critical("Config file is not valid JSON!")
         raise error
 
-def save(filename:str=FILENAME, manager=manager):
-    """Save config settings to FILENAME.
+def save(filename:str=FILE_CONFIG, manager=manager):
+    """Save JSON config settings to a file.
     
-    filename - The name of the file you wish to write to.
-    manager - The JSON data you want to write."""
+    filename - The name of the file you wish to write to, defaults to FILE_CONFIG.
+    manager - The JSON data you want to write, defaults to settings.manager.
+    """
     logger.info("Saving/creating config file.")
     try:
         with open(filename, "w") as f:
