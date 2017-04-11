f16bbb196cd334e916e9522b46187b6e3f7440b1
n303p4 <n303p4@users.noreply.github.com>
2017-04-11 18:00:01 -0400

    [cogs/eightball.py] Added missing quote, added periods.

--- cogs/eightball.py
+++ cogs/eightball.py
@@ -10,27 +10,28 @@ import random
 import discord
 from discord.ext import commands
 
-ANSWERS = ["It it certain",
-           "It is decidedly so",
-           "Without a doubt",
-           "Yes definitely",
-           "You may rely on it",
-           "As I see it, yes",
-           "Most likely",
-           "Outlook good",
-           "Yes",
-           "Signs point to yes",
+ANSWERS = ["It it certain.",
+           "It is decidedly so.",
+           "Without a doubt.",
+           "Yes definitely.",
+           "You may rely on it.",
+           "As I see it, yes.",
+           "Most likely.",
+           "Outlook good.",
+           "Yes.",
+           "Signs point to yes.",
            
-           "Reply hazy try again",
-           "Ask again later",
-           "Better not to tell you now",
-           "Cannot predict now",
-           "Concentrate and ask again",
+           "Reply hazy try again.",
+           "Ask again later.",
+           "Better not to tell you now.",
+           "Cannot predict now.",
+           "Concentrate and ask again.",
            
-           "Don't count on it",
-           "My reply is no",
-           "Outlook not so good",
-           "Very doubtful"]
+           "Don't count on it.",
+           "My reply is no.",
+           "My sources say no.",
+           "Outlook not so good.",
+           "Very doubtful."]
 
 logger = logging.getLogger(__name__)
 
