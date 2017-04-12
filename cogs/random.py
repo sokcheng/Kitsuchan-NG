7837263ab2ad84eead228401af139580cbfc3056
n303p4 <n303p4@users.noreply.github.com>
2017-04-11 23:37:49 -0400

    [cogs/random.py] Code comments since this thing isn't super intuitive.

--- cogs/random.py
+++ cogs/random.py
@@ -23,6 +23,7 @@ MAX_DIE_SIZE = 2000
 
 logger = logging.getLogger(__name__)
 
+# Instantiate a SystemRandom object for cryptographically secure random number generation.
 systemrandom = random.SystemRandom()
 
 class Utilities:
@@ -56,44 +57,61 @@ class Utilities:
         Examples:
         * roll 5d6 - Roll five six sided dice.
         * roll 1d20 2d8 - Roll one twenty sided die, and two eight sided dice."""
+        
+        # The former list nests sublists of format [expression, num dice, num sides per die]
+        # For example, ["5d6", 5, 6]
+        
+        # The latter list nests sublists of format [expression, outcome 1, outcome 2, ...]
+        # For example, ["5d6", 1, 6, 3, 4, 3]
         list_roll_parameters = []
         list_rolls = []
-        # Message to be sent.
+        
+        # Message to be sent. Instantiated as None initially.
         message = None
+        
         for expression in expressions:
             if isinstance(expression, str) and REGEX_OBJECT_DND.fullmatch(expression):
                 expression_parts = re.split(REGEX_DND_SPLIT, expression)
+                # Split the expression into two ints, one for the no. of dice and one for sides.
+                # After that, insert the original expression into the front.
                 roll_parameters = [int(value) for value in expression_parts]
                 roll_parameters.insert(0, expression)
                 list_roll_parameters.append(roll_parameters)
-        # All rolls were invalid.
+        
         if len(list_roll_parameters) == 0:
             message = "No valid rolls given; please use D&D format."
             raise commands.UserInputError(message)
-        # Too many rolls requested.
+        
         elif len(list_roll_parameters) > MAX_ROLL_COUNT:
             message = "Too many rolls requested."
             raise commands.UserInputError(message)
-        # Only some rolls were invalid.
+        
         elif len(list_roll_parameters) != len(expressions):
             message = "Some rolls have been ignored, as they were invalid."
+        
         for roll_parameters in list_roll_parameters:
             # Skip overly massive rolls.
             if roll_parameters[1] > MAX_DICE_PER_ROLL or roll_parameters[2] > MAX_DIE_SIZE:
                 message = "Some rolls have been ignored, as they were too large."
                 continue
+            
+            # Actually roll the dice now.
             roll = [roll_parameters[0]]
             for times in range(roll_parameters[1]):
                 roll.append(systemrandom.randint(1, roll_parameters[2]))
             list_rolls.append(roll)
+        
         if len(list_rolls) == 0:
             message = "Your rolls have been ignored, as they were too large."
             raise commands.UserInputError(message)
+        
         embed = discord.Embed(title="Rolls")
+        
         for roll in list_rolls:
             roll_string = ", ".join([str(value) for value in roll[1:]])
             roll_sum_string = str(sum(roll[1:]))
             embed.add_field(name=f"{roll_string} ({roll_sum_string})", value=roll[0])
+        
         logger.info("Rolling some dice.")
         await ctx.send(message, embed=embed)
 
