ca3f90c37b79ea349ac4e727d47d00e03e712e0f
n303p4 <n303p4@users.noreply.github.com>
2017-05-02 03:36:26 -0400

    [cogs/google.py] Image functionality working.

--- cogs/google.py
+++ cogs/google.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-"""This cog contains some basic xkcd commands."""
+"""This cog contains Google functionality. It is AWFUL."""
 
 # Standard modules
+import json
 import logging
 import random
 import re
@@ -15,19 +16,20 @@ from discord.ext import commands
 # Bundled modules
 import helpers
 
+systemrandom = random.SystemRandom()
+
 logger = logging.getLogger(__name__)
 
 # Constants
 BASE_URL_GOOGLE = "https://www.google.com/search?q={0}"
 BASE_URL_GOOGLE_IMAGES = "https://www.google.com/search?q={0}&tbm=isch"
 
-USER_AGENT = "NetSurf/3.6 (Linux, Ubuntu)"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0"
 HEADERS = {"User-Agent": USER_AGENT}
 
 PATTERN_URL_START = "^http(s)://"
 
 class Google:
-    """xkcd command."""
 
     async def _google(self, ctx, *, query:str, base_url=BASE_URL_GOOGLE):
         """Helper function for Google.
@@ -39,20 +41,27 @@ class Google:
             if response.status == 200:
                 data = await response.text()
                 soup = BeautifulSoup(data)
-                links = soup.find_all("cite", class_=None)
-                for index in range(len(links)):
-                    full_link = []
-                    for content in links[index].contents:
-                        content = str(content).replace("<b>", "").replace("</b>", "")
-                        full_link.append(content)
-                    full_link = "".join(full_link)
-                    if not re.match(PATTERN_URL_START, full_link):
-                        full_link = "http://" + full_link
-                    links[index] = full_link
-                see_also = [f"<{link}>" for link in links[1:4]]
-                see_also = "\n".join(see_also)
-                message = f"{links[0]}\n\n**You may also want to look at:**\n{see_also}"
-                await ctx.send(message)
+                if base_url == BASE_URL_GOOGLE:
+                    links = soup.find_all("cite", class_="_Rm")
+                    for index in range(len(links)):
+                        full_link = []
+                        for content in links[index].contents:
+                            content = str(content).replace("<b>", "").replace("</b>", "")
+                            full_link.append(content)
+                        full_link = "".join(full_link)
+                        if not re.match(PATTERN_URL_START, full_link):
+                            full_link = "http://" + full_link
+                        links[index] = full_link
+                    see_also = [f"<{link}>" for link in links[1:4]]
+                    see_also = "\n".join(see_also)
+                    message = f"{links[0]}\n\n**You may also want to look at:**\n{see_also}"
+                    await ctx.send(message)
+                else:
+                    links = soup.find_all("div", class_="rg_meta")
+                    for index in range(len(links)):
+                        links[index] = json.loads(links[index].contents[0]).get("ou")
+                    link = systemrandom.choice(links)
+                    await ctx.send(link)
             else:
                 message = "Could not reach Google. x.x"
                 await ctx.send(message)
@@ -60,10 +69,12 @@ class Google:
     
     @commands.group(aliases=["g"], invoke_without_command=True)
     async def google(self, ctx, *, query:str):
+        """Search Google."""
         await self._google(ctx, query=query)
 
     @google.command(aliases=["i"])
-    async def image(self, ctx, *, query:str,):
+    async def image(self, ctx, *, query:str):
+        """Search Google Images."""
         await self._google(ctx, query=query, base_url=BASE_URL_GOOGLE_IMAGES)
 
 def setup(bot):
