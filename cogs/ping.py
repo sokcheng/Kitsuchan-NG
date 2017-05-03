75162d6755b6982c5f48a7590da09bf42ecec6ee
n303p4 <n303p4@users.noreply.github.com>
2017-05-03 13:28:36 -0400

    [cogs/ping.py] BANG

--- cogs/ping.py
+++ cogs/ping.py
@@ -3,14 +3,25 @@
 # Standard modules
 import datetime
 import logging
+import random
 
 # Third party modules
+import discord
 from discord.ext import commands
+
+# Bundled modules
+import helpers
+
+systemrandom = random.SystemRandom()
 
 logger = logging.getLogger(__name__)
 
 VARIANTS = {"bang": ":gun: **BANG!**",
             "bang!": ":gun: **BANG!**"}
+GUNS = ("http://safebooru.org/samples/79/sample_d848db3fbe3fc8dc0f2cbb328f3af71c4e49989f.jpg",
+        "http://safebooru.org/images/121/af2b15dbcd0a8feac03a4b990a600837724c84ec.jpg",
+        "http://safebooru.org/images/1652/3f6d218b2433c9fe4e80f29a1a95eace88188f87.png",
+        "http://safebooru.org/images/1591/ff08983843fe399487629927d2e10bc8a9dc51d2.jpg")
 
 class Ping:
     """Ping command."""
@@ -24,7 +35,15 @@ class Ping:
             variant = VARIANTS[ctx.invoked_with]
         else:
             variant = ":ping_pong:"
-        await ctx.send(f"{variant} {pingtime} ms!")
+        message = f"{variant} {pingtime} ms!"
+        embed = None
+        if "bang" in ctx.invoked_with:
+            if not helpers.has_scanning(ctx):
+                embed = discord.Embed()
+                embed.set_image(url=systemrandom.choice(GUNS))
+            else:
+                message = "\n".join((message, systemrandom.choice(GUNS)))
+        await ctx.send(message, embed=embed)
         logger.info(f"Ping! False ping time: {pingtime} ms")
 
 def setup(bot):
