bdd8951a3be683524cef6631ffb082198df87f50
n303p4 <n303p4@users.noreply.github.com>
2017-05-02 23:36:32 -0400

    [KIMP --> cogs/memes.py] Renamed and stuff.

--- cogs/memes.py
+++ cogs/memes.py
@@ -7,9 +7,9 @@ from discord.ext import commands
 
 import logging
 
-BASE_URL_KIMP = "https://n303p4.github.io/{0}.html?{1}"
+BASE_URL_MEME = "https://n303p4.github.io/{0}.html?{1}"
 
-class KIMP:
+class Memes:
 
     def mogrify(self, template:str, **kwargs):
         """Meme generation.
@@ -18,26 +18,28 @@ class KIMP:
         * **args - The arguments to use in the format.
         """
         params = urllib.parse.urlencode(kwargs)
-        url = BASE_URL_KIMP.format(template, params)
+        url = BASE_URL_MEME.format(template, params)
         return url
 
-    @commands.command()
+    @commands.command(aliases=["um", "standard"])
     @commands.cooldown(6, 12, commands.BucketType.user)
-    async def standard(self, ctx, top:str, bottom:str, *, member:discord.Member=None):
-        """Standard meme. Use quotes around your arguments.
+    async def usermeme(self, ctx, top:str, bottom:str, *, member:discord.Member=None):
+        """Create a meme of a user. Use quotes around your arguments.
         
         Example usage:
         
-        * kit standard \"This is\" "A meme\"
-        * kit standard \"This is\" "A meme\" @Kitsuchan"""
+        * kit usermeme \"This is\" "A meme\"
+        * kit usermeme \"This is\" "A meme\" @Kitsuchan
+        """
         if not member:
             member = ctx.author
         url_avatar = member.avatar_url.replace(".webp", ".png")
-        url = self.mogrify("standard", title=" / ".join((top, bottom)), image=url_avatar, top=top, bottom=bottom)
+        url = self.mogrify("standard", title=" / ".join((top, bottom)),
+                           image=url_avatar, top=top, bottom=bottom)
         embed = discord.Embed(title=f"{member.display_name} as a standard meme!")
         embed.description = f"[Click here to view]({url})"
         await ctx.send(embed=embed)
 
 def setup(bot):
     """Setup function."""
-    bot.add_cog(KIMP())
+    bot.add_cog(Memes())
