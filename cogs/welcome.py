a8718623089d82f9f8faf05ebab7b3658cca37b4
n303p4 <n303p4@users.noreply.github.com>
2017-05-03 12:40:56 -0400

    [cogs/welcome.py] Welcome messages. I suggest you don't use this.

---
+++ cogs/welcome.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""This cog contains welcome messages. You probably shouldn't use this."""
+
+# Standard modules
+import logging
+
+# Third-party modules
+from discord.ext import commands
+
+logger = logging.getLogger(__name__)
+
+class Welcome:
+    """Welcome messages."""
+
+    def __init__(self, bot):
+    
+        @bot.add_to_event("on_member_join")
+        async def welcome(member):
+            guild = member.guild
+            await guild.default_channel.send((f"Welcome to **{guild.name}**, {member.mention}. "
+                                              "Hope you enjoy your stay! :D"))
+
+def setup(bot):
+    """Setup function for Wikipedia."""
+    bot.add_cog(Welcome(bot))
