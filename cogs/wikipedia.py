fc9f4a0f7645e080200437c8c24d08d4a40cefbe
n303p4 <n303p4@users.noreply.github.com>
2017-05-05 03:47:07 -0400

    [cogs/dictionary.py, cogs/wikipedia.py] Results are no longer inline.

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -42,7 +42,7 @@ class Wikipedia:
                 embed = discord.Embed()
                 for index in range(0, min(3, len(data[1]))):
                     description = f"{data[3][index]}\n{data[2][index]}"
-                    embed.add_field(name=data[1][index], value=description)
+                    embed.add_field(name=data[1][index], value=description, inline=False)
                 await ctx.send(embed=embed)
                 logger.info("Data retrieved!")
             else:
