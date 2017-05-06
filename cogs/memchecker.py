d2c110a96b61cbdf4e124db1c4441b9ea5e33e7b
n303p4 <n303p4@users.noreply.github.com>
2017-05-06 04:23:06 -0400

    [cogs/memchecker.py] Logs memory usage to logging channels.

---
+++ cogs/memchecker.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import resource
+
+from discord.ext import commands
+
+class Memchecker:
+
+    def __init__(self, bot):
+    
+        self.previous_memory_usage = 0
+        
+        @bot.add_to_event("on_command_completion")
+        async def check_memory(ctx):
+            memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            if memory_usage != self.previous_memory_usage:
+                for channel in bot.logging_channels:
+                    await channel.send(f"Memory usage is now {memory_usage} KB")
+                self.previous_memory_usage = memory_usage
+
+def setup(bot):
+    bot.add_cog(Memchecker(bot))
