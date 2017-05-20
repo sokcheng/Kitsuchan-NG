6c0c43d8fc1868a0dd0a39db8aa1017e81ca9afa
n303p4 <n303p4@users.noreply.github.com>
2017-05-20 12:19:34 -0400

    [cogs/discrim.py] Reverted old behavior.

--- cogs/discrim.py
+++ cogs/discrim.py
@@ -26,27 +26,25 @@ class Discriminator:
         * discriminator - A discriminator to search for."""
         if not discriminator:
             discriminator = str(ctx.author.discriminator)
-        for search_area, area_type in ((ctx.guild.members, "in this guild"),
-                                       (ctx.bot.users, "in all my guilds")):
-            results = []
-            for member in search_area:
-                if str(member.discriminator) == discriminator:
-                    results.append(f"{len(results)+1}. {member.name}#{member.discriminator}")
-            if len(results) == 0:
-                await ctx.send(f"Could not find any members {area_type} with that discriminator.")
-                continue
-            paginator = commands.Paginator(prefix="```markdown")
-            if len(results) > 1:
-                plural = "s"
-            else:
-                plural = ""
-            paginator.add_line(f"# Found {len(results)} member{plural} {area_type} ({discriminator}) #")
-            for member in results[:10]:
-                paginator.add_line(member)
-            if len(results) > 10:
-                paginator.add_line(f"...and {len(results)-10} others.")
-            for page in paginator.pages:
-                await ctx.send(page)
+        results = []
+        for member in ctx.guild.members:
+            if str(member.discriminator) == discriminator:
+                results.append(f"{len(results)+1}. {member.name}#{member.discriminator}")
+        if len(results) == 0:
+            await ctx.send(f"Could not find any members in this guild with that discriminator.")
+            return
+        paginator = commands.Paginator(prefix="```markdown")
+        if len(results) > 1:
+            plural = "s"
+        else:
+            plural = ""
+        paginator.add_line(f"# Found {len(results)} member{plural} in this guild ({discriminator}) #")
+        for member in results[:10]:
+            paginator.add_line(member)
+        if len(results) > 10:
+            paginator.add_line(f"...and {len(results)-10} others.")
+        for page in paginator.pages:
+            await ctx.send(page)
 
 def setup(bot):
     """Setup function for Utilities."""
