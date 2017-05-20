9e2408864934058650cc50eeed050fd03cf75c23
n303p4 <n303p4@users.noreply.github.com>
2017-05-20 11:20:52 -0400

    [cogs/bingwp.py] Well dang it actually works now.

--- cogs/bingwp.py
+++ cogs/bingwp.py
@@ -1,48 +1,43 @@
 #!/usr/bin/env python3
 
+import random
+
+from discord.ext import commands
+
+BASE_URL_BING_API = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n={0}&mkt=en-US"
+BASE_URL_BING = "https://www.bing.com{0}"
+
+systemrandom = random.SystemRandom()
 
 class Wallpapers:
 
     @commands.command(aliases=["bwp"])
-    async def bingwp(self, ctx, number:int=None):
-        """Query Bing for a wallpaper. Optional number of wallpapers."""
-        logger.info("Querying Bing for wallpaper(s).")
-        # The number of wallpapers desired.
-        number = 1000
-        # Random wallpaper bool.
-        random_mode = True
-        # If there are arguments...
-        if len(args) > 0:
-            # See if the last argument can be used as a number.
-            if str(args[-1]).isdigit():
-                number = int(args[-1])
-                # Hard cap the limit at 8, which is the maximum allowed.
-                if number > 8:
-                    number = 8
-                random_mode = False
-            if args[-1].lower() == "random":
-                pass
-            elif args[-1].lower() in ("today", "latest", "current"):
-                random_mode = False
-                number = 1
-        # Query Bing's JSON API.
-        url = "https://www.bing.com/HPImageArchive.aspx?" +\
-              "format=js&idx=0&n={number}&mkt=en-US" % (number,)
-        # Grab the thing.
+    @commands.cooldown(6, 12, commands.BucketType.channel)
+    async def bingwp(self, ctx, wallpaper_count:int=None):
+        """Query Bing for a wallpaper. Optional number of wallpapers.
+        
+        * wallpaper_count - The number of wallpapers requested."""
+        
+        url = BASE_URL_BING_API.format(wallpaper_count if wallpaper_count else 1000)
+
         async with ctx.bot.session.get(url) as response:
             if response.status == 200:
                 data = await response.json()
             else:
                 message = "Could not fetch URL."
                 raise commands.UserInputError(message)
-        # Nonrandom mode.
-        if not random_mode:
+
+        if wallpaper_count:
             quote = []
-            for index in range(len(data["images"])):
-                url = "https://www.bing.com" + data["images"][index]["url"]
+            for index in range(0, min(len(data["images"]), wallpaper_count)):
+                url = BASE_URL_BING.format(data["images"][index]["url"])
                 quote.append(url)
             quote = "\n".join(quote)
         else:
-            image = random.choice(data["images"])
-            quote = "https://www.bing.com" + image["url"]
-        await kitsuchan.say(quote)
+            image = systemrandom.choice(data["images"])
+            quote = BASE_URL_BING.format(image["url"])
+        
+        await ctx.send(quote)
+
+def setup(bot):
+    bot.add_cog(Wallpapers())
